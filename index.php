c845472072b2691570ea80192083a960205574e7
Dario Berzano <dario.berzano@cern.ch>
2014-03-10 23:47:04 +0100

    Fixed version number

--- index.php
+++ index.php
@@ -82,7 +82,7 @@ $serverFqdn = $_SERVER['SERVER_NAME'];
 $authValid = false;
 
 // Version
-$authVer = '0.8.6';
+$authVer = '0.9.0';
 
 // Error messages are an array, empty at start
 $errMsg = array();
